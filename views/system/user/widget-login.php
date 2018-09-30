bb595c4fd516c9f4c4a54c3935b4f64d38be0732
Fosphatic-Duke <f.duke@web.de>
2018-09-30 10:17:49 +0200

    fix

--- views/system/user/widget-login.php
+++ views/system/user/widget-login.php
@@ -9,15 +9,15 @@
 <form class="uk-form" action="<?= $view->url('@user/authenticate') ?>" method="post">
 
     <div class="uk-margin">
-        <input class="uk-input uk-form-width-large uk-width-3-4" type="text" name="credentials[username]" value="<?= $this->escape($last_username) ?>" placeholder="<?= __('username') ?>">
+        <input class="uk-input uk-form-width-large uk-width-1-2@m uk-width-1-4@l" type="text" name="credentials[username]" value="<?= $this->escape($last_username) ?>" placeholder="<?= __('username') ?>">
     </div>
 
     <div class="uk-margin">
-        <input class="uk-input uk-form-width-large uk-width-3-4" type="password" name="credentials[password]" value="" placeholder="<?= __('password') ?>">
+        <input class="uk-input uk-form-width-large uk-width-1-2@m uk-width-1-4@l" type="password" name="credentials[password]" value="" placeholder="<?= __('password') ?>">
     </div>
 
     <div class="uk-margin">
-        <button class="uk-button uk-button-primary uk-width-1-3" type="submit"><?= __('Login') ?></button>
+        <button class="uk-button uk-button-primary" type="submit"><?= __('Login') ?></button>
     </div>
 
     <p>
